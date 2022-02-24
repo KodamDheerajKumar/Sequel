b3e95574c31c38350c365a0ce0c4dc67eaac82e0
DheerajKumarKodam <58454961+KodamDheerajKumar@users.noreply.github.com>
2022-02-24 10:09:06 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,10 +5,4 @@ module.exports = {
     DB: 'sequelize',
     dialect: 'mysql',
 
-    // pool: {
-    //     max: 5,
-    //     min: 0,
-    //     acquire: 30000,
-    //     idle: 10000
-    // }
 }
